28a499130651349a0df371118b35a9b244863bab
Karl Koscher <supersat@cs.washington.edu>
2014-04-29 19:29:39 -0700

    More intelligent movement

--- robovator.py
+++ robovator.py
@@ -32,6 +32,17 @@ class Robovator:
         self.floor_selected = 0;
         self.cmd_queue = Queue(2)
 
+        self.mode = 'XX'
+        self.RD = False
+        self.PR = False
+        self.R5 = False
+        self.R4 = False
+        self.R3 = False
+        self.R2 = False
+        self.R1 = False
+        self.R0 = False
+        self.last_floor = 1
+
     def read(self):
         c = self.ser.read()
         if c != '\x05':
@@ -60,16 +71,59 @@ class Robovator:
         floor = int(floor)
         sys.stderr.write('Going to floor %s\n' % (floor))
         if floor < self.floor_selected:
+            while self.mode == 'UP':
+                self.update_status()
             for x in range(0, (self.floor_selected - floor)):
                 self.ser.write('\x0a')
                 time.sleep(0.05)
         elif floor > self.floor_selected:
+            while self.mode == 'DN':
+                self.update_status()
             for x in range(0, (floor - self.floor_selected)):
                 self.ser.write('\x0b')
                 time.sleep(0.05)
         self.ser.write('\x0d')
         self.floor_selected = floor
+        while self.last_floor != self.floor_selected:
+            self.update_status()
         sys.stderr.write('Done issuing command\n')
+
+    def update_status(self):
+        c = self.read()
+        if c == '\x05':
+            self.ser.write('\x06')
+        elif c == '\x1b':
+            c = self.read()
+            if c == '\x3b':
+                y = self.read()
+                x = self.read()
+                if (y == '\x29') and (x == '\x4a'):
+                    self.mode = self.read() + self.read()
+                elif (y == '\x28'):
+                    if (x == '\x22'):
+                        self.RD = (self.read() == '\x1b')
+                    elif (x == '\x25'):
+                        self.PR = (self.read() == '\x1b')
+                    elif (x == '\x2a'):
+                        self.R5 = (self.read() == '\x1b')
+                    elif (x == '\x2d'):
+                        self.R4 = (self.read() == '\x1b')
+                    elif (x == '\x30'):
+                        self.R3 = (self.read() == '\x1b')
+                    elif (x == '\x33'):
+                        self.R2 = (self.read() == '\x1b')
+                    elif (x == '\x36'):
+                        self.R1 = (self.read() == '\x1b')
+                    elif (x == '\x39'):
+                        self.R0 = (self.read() == '\x1b')
+                if self.RD:
+                    self.last_floor = \
+                        (32 if self.R5 else 0) + \
+                        (16 if self.R5 else 0) + \
+                        (8 if self.R5 else 0) + \
+                        (4 if self.R5 else 0) + \
+                        (2 if self.R5 else 0) + \
+                        (1 if self.R5 else 0) - 1                        
 
     def loop(self):
         self.wait_for_term_req()
@@ -85,9 +139,7 @@ class Robovator:
         sys.stderr.write('Got hoistway view\n')
 
         while True:
-            c = self.read()
-            if c == '\x05':
-                self.ser.write('\x06')
+            self.update_status()
             if not self.cmd_queue.empty():
                 sys.stderr.write('Got a command\n')
                 self.go_to_floor(self.cmd_queue.get())
